c08d8ea9db66d570f6ff78066c62251618606fe4
indrajba <indrajba@amdocs.com>
2025-06-02 23:33:02 +0530

    Updates

--- src/ProtfolioContainer/Common/Page/PageSection.js
+++
@@ -1,9 +0,0 @@
-const PageSection = (props) => {
-    return(
-        <section className = {`page-section ${props.className} p-3 p-lg-5`}>
-            {props.body}
-        </section>
-    );
-}
-
-export default PageSection;
